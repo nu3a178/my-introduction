21e30ed89de5f66f4c095f9f0b2c7ba73677e5ff
nu3a178 <nu197ma@gmail.com>
2023-12-12 09:10:15 +0900

    231212 spotifyWebAPIを追加

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 
 #-----基本形-----
-from flask import Flask
+from flask import Flask,request,jsonify
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+import csv
+
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__ )
 
@@ -9,6 +18,62 @@ app = Flask(__name__ )
 def index():
     return render_template('index.html')
 
+#--------------
+
+@app.route("/spotifyAuthLogin")
+def spotifyAuthLogin():
+    return render_template('spotifyAuthLogin.html')
+
+@app.route("/spotifyAuthCallback")
+def spotifyAuthCallback():
+    return render_template('spotifyAuthCallback.html')
+
+@app.route("/saveToken",methods=["POST"])
+def saveToken():
+    data = request.get_json()
+    tokens = {
+      "accessToken" :data.get('access_token'),
+      "refreshToken" : data.get('refresh_token'),
+      "getDate" : data.get('get_date')
+    }
+    saveTokens(tokens)
+    return "success"
+
+@app.route("/updateToken",methods=["POST"])
+def updateToken():
+    data = request.get_json()
+    tokens = {
+      "accessToken" :data.get('access_token'),
+      "refreshToken" : data.get('refresh_token'),
+      "getDate" : data.get('get_date')
+    }
+    print(tokens)
+    saveTokens(tokens)
+    return "success"
+
+@app.route("/getToken")
+def getToken():
+    with open('spotify_tokens.csv','r')as csvfile:
+        csvreader = csv.reader(csvfile)
+        headers = next(csvreader)
+        next(csvreader)
+        data = next(csvreader)
+        tokens = {
+          "access_token" :data[0],
+          "refresh_token" : data[1],
+          "get_date" : data[2]
+        }
+        return jsonify(tokens)
+    
+
+def saveTokens(tokens):
+    with open('spotify_tokens.csv','w')as csvfile:
+        fieldnames=["accessToken","refreshToken","getDate"]
+        csvwriter = csv.DictWriter(csvfile,fieldnames=fieldnames)
+        csvwriter.writeheader()
+        csvwriter.writerow(tokens)
+    
+
 if __name__=='__main__':
     app.run()
-#--------------
+
---
+++ static/js/spotify_api.js
@@ -0,0 +1,129 @@
+
+const clientId = '44ea08153660400f8d37a81b9d862207';
+const clientSecret = 'ddbcab3a0d6d458db71da8a9b65955fd';
+const redirectUri = 'http://localhost:5000/spotifyAuthCallback';
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+const tokenEndpoint = 'https://accounts.spotify.com/api/token';
+const scopes = ['user-read-recently-played'];
+
+
+const basicAuth = btoa(`${clientId}:${clientSecret}`);
+
+function authorizeSpotify() {
+    window.location.href = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=code`;
+}
+
+//spotifyWebApiから取得した認証コードをtokens{access_token,refresh_token,get_date}に変換する。
+async function exchangeCodeToTokens(code){
+    const params = new URLSearchParams({
+        code: code,
+        redirect_uri: redirectUri,
+        grant_type: 'authorization_code'
+    });
+
+    var options = {
+        method:"POST",
+        body: params.toString(),
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + basicAuth
+        },
+      };
+
+    const data = await fetch(tokenEndpoint,options).then(response=> response.json()).then(data=>{return data});
+    d = new Date();
+    const tokens ={
+        accessToken:data.access_token,
+        refreshToken:data.refresh_token,
+        getDate: d.getTime()
+    }
+    return tokens
+}
+
+//引数のtokensをweb上のタグに設定する。
+function setTokensToForm(tokens){
+    document.getElementById("access_token").value=tokens.accessToken
+    document.getElementById("refresh_token").value=tokens.refreshToken
+    document.getElementById("get_date").value=tokens.getDate
+}
+
+//引数のtokenでサーバのcsvファイルを上書きする。
+function updateTokens(tokens){
+    body = JSON.stringify(tokens);
+    fetch("/updateToken",{
+        method:"POST",
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: body.toString(),
+    });
+}
+
+//最近聞いたSpotifyの曲を取得する。
+async function getRecentlyPlayedTracks() {
+
+    //サーバの'/getToken'にGETリクエストを送り、spotify_tokens.csvファイルに記されているトークン情報を取得する。
+    var tokens = await fetch("/getToken",{
+        method:"GET",
+    }).then(response=> {
+        console.log(response)
+        return response.json()
+    }).then(data=>data).catch(error=>console.log(error));
+    console.log("getToken直後",tokens);
+
+    //現在時間がトークンの取得時刻から1時間以上経っている場合、refreshトークンを使ってWeb APIからaccessトークンを再取得する。
+    var now = new Date().getTime()
+    if (now - tokens.get_date>3600000 ){
+
+        const body = new URLSearchParams({
+            grant_type:'refresh_token',
+            refresh_token:tokens.refresh_token,
+        });
+        console.log("この内容でPOSTします",body.toString())
+        data = await fetch(tokenEndpoint,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + basicAuth
+            },
+            body: body.toString(),
+        }).then(response=>response.json()).then(data=>data);
+        console.log("webAPIから取得したtoken",data);
+        
+        
+        tokens.access_token = data.access_token;
+        tokens.get_date = new Date().getTime();
+        
+        console.log("updateをかけるtokenの内容",tokens)
+        await updateTokens(tokens)
+
+    }else{
+        console.log("less than 1hour")
+    }
+
+    const recentlyPlayedUrl = 'https://api.spotify.com/v1/me/player/recently-played';
+    access_token = tokens.access_token
+    fetch(recentlyPlayedUrl, {
+        headers: {
+            'Authorization': `Bearer ${access_token}`
+        }
+    })
+        .then(response => response.json())
+        .then(data => displayTracks(data.items))
+        .catch(error => console.error('Error:', error));
+}
+
+function displayTracks(tracks) {
+    const tracksDiv = document.getElementById('tracks');
+    tracksDiv.innerHTML = ""
+    tracks.forEach(item => {
+        const trackName = item.track.name;
+        const artistName = item.track.artists[0].name;
+        const trackInfo = `<div class="card col-4" ><p>Track: ${trackName}</p><p> Artist: ${artistName}</p></div>`;
+        tracksDiv.innerHTML += trackInfo;
+    });
+}
+
+
+
+
